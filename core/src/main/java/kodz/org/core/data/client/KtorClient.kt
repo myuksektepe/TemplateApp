48dac8e39d9886ec9f3b7ef44f9e144f6fae6e85
myuksektepe <yuksektepemurat@gmail.com>
2024-03-05 21:49:38 +0300

    Ktor logging fun added

--- core/src/main/java/kodz/org/core/data/client/KtorClient.kt
+++ core/src/main/java/kodz/org/core/data/client/KtorClient.kt
@@ -1,17 +1,16 @@
 package kodz.org.core.data.client
 
+import android.util.Log
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.header
 import io.ktor.serialization.gson.gson
 import io.ktor.serialization.kotlinx.json.json
-
 import kodz.org.core.domain.consts.BASE_URL
 
 val ktorClient = HttpClient(CIO) {
@@ -27,7 +26,11 @@ val ktorClient = HttpClient(CIO) {
     }
 
     install(Logging) {
-        logger = Logger.DEFAULT
+        logger = object : Logger {
+            override fun log(message: String) {
+                Log.d("KTOR", message)
+            }
+        }
         level = LogLevel.ALL
     }
 
